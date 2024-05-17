93d3156580d11ef9c02715b264b115bde5d15571
bibichannel <lytrung15901@gmail.com>
2024-05-17 15:32:47 +0700

    update

--- main.py
+++ main.py
@@ -4,21 +4,11 @@ import pandas as pd
 import streamlit as st
 import socket
 
-
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
 hostname=socket.gethostname()
 IPAddr=socket.gethostbyname(hostname)
 
-st.subheader("Your Computer IP Address is:" + IPAddr)
+st.header("Welcome to Streamlit!")
+st.subheader("Your Computer IP Address is: " + IPAddr)
 
 num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
 num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
